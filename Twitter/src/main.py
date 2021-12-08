c5be8d062743c6058c391d354bac411741e1f0e8
Serithipithy <alexandramosoiu19@gmail.com>
2021-12-08 22:38:16 +0200

    get what data is used from twitter

--- Twitter/src/main.py
+++ Twitter/src/main.py
@@ -1,12 +1,13 @@
 from src.services.TwitterService import *
 
 if __name__ == '__main__':
-    hashtag = "#" + input("Please enter a hashtag you wanna see: ")
-    nr_tweets = int(input("How many tweets would you like to see? "))
+    # hashtag = "#" + input("Please enter a hashtag you wanna see: ")
+    # nr_tweets = int(input("How many tweets would you like to see? "))
 
     tw_service = TwitterService()
 
-    tw_service.search(hashtag, nr_tweets)
+    # tw_service.search(hashtag, nr_tweets)
+    tw_service.search("v", 20)
     tw_service.get_content()
 
 
--- Twitter/src/services/TwitterService.py
+++ Twitter/src/services/TwitterService.py
@@ -1,22 +1,36 @@
 from src.configure.connection import *
+import json
 
 
 class TwitterService:
     api = None
     searched_tweets = None
     content = None
+    max_tweets = None
 
     def __init__(self):
         self.api = auth_twitter()
 
     def search(self, query: str, max_tweets: int):
-        self.max_tweets=max_tweets
+        self.max_tweets = max_tweets
         self.searched_tweets = tweepy.Cursor(self.api.search_tweets,
                                              q=query,
                                              result_type="recent",
-                                             count=max_tweets)
+                                             count=max_tweets,
+                                             include_entities="include_entities")
 
     def get_content(self):
+        tweets = list()
         for tweet in self.searched_tweets.items(self.max_tweets):
-            # print(tweet.text)
-            print(tweet.coordinates)
+            content = {
+                "text": tweet.text,
+                "author": tweet.author.name,
+                "author_location": tweet.author.location,
+                "coordinates": tweet.coordinates,
+                "place": tweet.place,
+                "geo": tweet.geo
+            }
+
+            tweets.append(content)
+        for line in tweets:
+            print(line["author_place"])
