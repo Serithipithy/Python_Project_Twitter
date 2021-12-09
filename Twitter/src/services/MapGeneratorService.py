af84211a5ca1f12f3d83f9a928700753841162c6
Serithipithy <alexandramosoiu19@gmail.com>
2021-12-09 23:01:43 +0200

    change width for popup

--- Twitter/src/services/MapGeneratorService.py
+++ Twitter/src/services/MapGeneratorService.py
@@ -10,20 +10,21 @@ def mark_on_map(info):
     if info["coordinates"][0] != 0 or info["coordinates"][1] != 0:
         folium.Marker([info["coordinates"][0], info["coordinates"][1]],
                       popup=folium.Popup(
-                          html=f'<strong>{info["author"]}\'s tweet</strong>'
-                          f'</br>'
-                          f'<small><b>Created on: </b>{info["date"]["day"]}.{info["date"]["month"]}.{info["date"]["year"]}</small>'
-                          f'</br></br>'
-                          f'<small>{info["text"]}</small>',
-                          min_width="500px",
-                          max_width="500px"
+                          html='<style type="text/css" scoped> .leaflet-popup-content-wrapper { width: 250px }</style>'
+                               f'<strong>{info["author"]}\'s tweet</strong>'
+                               f'</br>'
+                               f'<small><b>Created on: </b>{info["date"]["day"]}.{info["date"]["month"]}.{info["date"]["year"]}</small>'
+                               f'</br></br>'
+                               f'<small>{info["text"]}</small>',
                       ),
                       tooltip=tooltip,
-                      icon=folium.Icon(icon="globe",color="lightblue")).add_to(map)
+                      icon=folium.Icon(icon="globe", color="lightblue")).add_to(map)
 
 
-with open("../data/tweets_info.json", "r", encoding="utf-8") as f:
-    data = json.load(f)
+def generate_map():
+    with open("../data/tweets_info.json", "r", encoding="utf-8") as f:
+        data = json.load(f)
     for chunk in data["info"]:
         mark_on_map(chunk)
     map.save("../../data/tweets_map.html")
+
